454708b3faa84a2e48d5cd68170b45fd0e17271a
RhythmAlam <91103938+RhythmAlam@users.noreply.github.com>
2025-11-11 00:39:47 -0500

    Add files via upload
    Authorization has the message 'user successfully created' now

--- static/scripts/authorization.js
+++ static/scripts/authorization.js
@@ -7,50 +7,57 @@ document.getElementById('btn_signup').addEventListener('click', async () => {
         return;
     }
 
-    const response = await fetch('/auth/signup', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password })
-        //credentials: 'include'
-    });
+    try {
+        const response = await fetch('/auth/signup', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password })
+            //credentials: 'include'
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (response.ok) {
-        messageDiv.textContent = data.message;
+        if (response.ok) {
+            alert(data.message || "User created successfully!");
+        } else {
+            alert(data.error || 'Signup failed');
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        alert('Something went wrong during signup.');
     }
-    else {
-        messageDiv.textContent = data.error || 'Signup Failed';
-    }
-
 });
+
 
 document.getElementById('btn_login').addEventListener('click', async () => {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
 
     if (!username || !password) {
         alert('Username and password is required');
         return;
     }
 
-    const response = await fetch('/auth/login', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password })
-        //credentials: 'include'
-    });
+    try {
+        const response = await fetch('/auth/login', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password })
+            //credentials: 'include'
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (response.ok) {
-        localStorage.setItem('access_token', data.access_token);
-        window.location.href = '/main';
+        if (response.ok) {
+            localStorage.setItem('access_token', data.access_token);
+            window.location.href = '/main';
+        } else {
+            alert(data.error || 'Login failed');
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        alert('Something went wrong during login.');
     }
-    else {
-        messageDiv.textContent = data.error || 'Login Failed';
-    }
-
 });
-
